f57bae624d3a0d5ebdc8d14a180f07176f6235b3
roofimon <roofimon@gmail.com>
2014-02-01 15:49:54 +0700

    Add unhandler

--- index.php
+++ index.php
@@ -1,2 +1,13 @@
 <?php
-  phpinfo();
+$app = new Phalcon\Mvc\Micro();
+
+$app->get('/say/welcome/{name}', function ($name) {
+    echo "<h1>Welcome $name!</h1>";
+});
+
+$app->notFound(function () use ($app) {
+    $app->response->setStatusCode(404, "Not Found")->sendHeaders();
+    echo 'This is crazy, but this page was not found!';
+});
+
+$app->handle();
--- tests/api/SearchListingCept.php
+++ tests/api/SearchListingCept.php
@@ -1,3 +1,4 @@
 <?php
 $I = new ApiGuy($scenario);
 $I->wantTo('perform actions and see result');
+$I->sendGet('say/hello/roofimon');
