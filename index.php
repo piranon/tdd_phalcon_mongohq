e6bc249f9e26b09b39991e4d6d0331006ec026d7
roofimon <roofimon@gmail.com>
2014-02-01 18:24:11 +0700

    Add curl for mongohq

--- index.php
+++ index.php
@@ -2,7 +2,17 @@
 $app = new Phalcon\Mvc\Micro();
 
 $app->get('/say/welcome/{name}', function ($name) {
-    echo "<h1>Welcome $name!</h1>";
+	$tuCurl = curl_init(); 
+	curl_setopt($tuCurl, CURLOPT_URL, "https://api.mongohq.com/databases/wine/collections/australian/documents?_apikey=9guoo8ndq9hj5y1ur1w1"); 
+	curl_setopt($tuCurl, CURLOPT_PORT , 443); 
+	curl_setopt($tuCurl, CURLOPT_RETURNTRANSFER, 1); 
+	$tuData = curl_exec($tuCurl); 
+	curl_close($tuCurl); 
+  $response = new Phalcon\Http\Response();
+
+  $response->setContentType('application/json');
+  $response->setContent($tuData);
+	return $response;
 });
 
 $app->notFound(function () use ($app) {
--- tests/api/SearchListingCept.php
+++ tests/api/SearchListingCept.php
@@ -1,4 +1,6 @@
 <?php
 $I = new ApiGuy($scenario);
 $I->wantTo('perform actions and see result');
-$I->sendGet('say/hello/roofimon');
+$I->sendGet('say/welcome/roofimon');
+$I->haveHttpHeader('Content-Type','application/json');
+$I->seeResponseCodeIs(200);
