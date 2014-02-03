653085be80fb796107d1d238505473d6dc0a7a11
roofimon <roofimon@gmail.com>
2014-02-03 11:37:21 +0700

    Extract wine controller

--- tests/api/SearchListingCept.php
+++ tests/api/SearchListingCept.php
@@ -2,5 +2,5 @@
 $I = new ApiGuy($scenario);
 $I->wantTo('perform actions and see result');
 $I->sendGet('api/v4/wine');
-$I->haveHttpHeader('Content-Type','application/json');
+$I->seeHttpHeader('Content-Type','application/json');
 $I->seeResponseCodeIs(200);
